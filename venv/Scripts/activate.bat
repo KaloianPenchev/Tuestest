4b1a368bbd3a7af84fea1bc1046aedaed93bcb5f
VictorT <victor.titov.2009@gmail.com>
2024-02-29 10:45:02 +0200

    why

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\VSProjects\_VSCode\HTML\Tuestest\venv
+set VIRTUAL_ENV=C:\Users\viti\source\repos\Tuestest\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
